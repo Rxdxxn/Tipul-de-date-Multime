0b6167dcf4de85db0f3560aa3dbf28ebd063f791
Rodion Balaniuc <70860169+Rxdxxn@users.noreply.github.com>
2021-11-17 18:43:43 +0200

    Add files via upload

--- prob10p60.py
+++ prob10p60.py
@@ -1,19 +1,17 @@
-#Problema
-import itertools
-from itertools import combinations, chain
-def submultimile(m, x):
-	return list(map(set, itertools.combinations(m, x)))
-def submultimile(m, y):
-	return list(map(set, itertools.combinations(m, y)))
-def submultimile(m, z):
-	return list(map(set, itertools.combinations(m, z)))
-m = {1, 2, 3, 4}
-x = 1
-y = 2 
-z = 3
-print("Submultimile cu lungimea 1->",submultimile(m, x))
-print("Submultimile cu lungimea 2->",submultimile(m, y))
-print("Submultimile cu lungimea 3->",submultimile(m, z))
-print("Submultimile cu lungimea 4->",m)
-
+A=set(input('dati elementele multimii A:'))
+B=set(input('dati elementele multimii B:'))
+a=set()
+b=set()
+for i in A:
+    if ord(str(i))>=48 and ord(str(i))<=57 :
+        a.add(int(i))
+for i in B:
+    if ord(str(i))>=48 and ord(str(i))<=57 :
+        b.add(int(i))
+    print("Reuniunea multimilor:",a.union(b))
+    print("Intersectia multimilor:",a.intersection(b))
+    print("Diferenta multimilor:",a.difference(b))
+    print("Diferenta multimilor:",b.difference(a))
+else:
+    print('Introduceti doar numere intergi!')
 
